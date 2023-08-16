4e105cc33fef1deab2bcece3d33990c15d9f49fe
Milan Verma <millan.verma@gmail.com>
2023-08-16 21:23:19 +0530

    updated code

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,18 +1,28 @@
-# Import convention
+from langchain.utilities import SQLDatabase
+from langchain.llms import OpenAI
+from langchain_experimental.sql import SQLDatabaseChain
 import streamlit as st
 
-st.title('SQL Chatbot 💬')
-st.file_uploader('Upload your sql files', type=['sql', 'sqlite'])
-if "messages" not in st.session_state:
-    st.session_state["messages"] = [{"role": "assistant", "content": "How can I help you?"}]
+st.title("SQL Chatbot")
+# Set up OpenAI API
+api_key = st.text_input("Enter your OpenAI API token")
 
-for msg in st.session_state.messages:
-    st.chat_message(msg["role"]).write(msg["content"])
+# File uploader
+uploaded_file = st.file_uploader("Upload SQL or SQLite file", type=["sql", "sqlite"])
 
-if prompt := st.chat_input():  
-    st.session_state.messages.append({"role": "user", "content": prompt})
-    st.chat_message("user").write(prompt)
-    response = llm_chain.run(st.session_state.messages)
-    msg = response.choices[0].message
-    st.session_state.messages.append(msg)
-    st.chat_message("assistant").write(msg.content)
+# Chat window
+query = st.text_input("Enter your query")
+
+# Submit query button
+if st.button("Submit Query"):
+    if uploaded_file is not None:
+        if api_key is not None:
+            llm = OpenAI(temperature=0, verbose=True, openai_api_key=api_key)
+            # Convert uploaded file to BytesIO object
+            db = SQLDatabase.from_uri("sqlite:///Chinook_SQLite")
+            db_chain = SQLDatabaseChain.from_llm(llm, db, verbose=True)
+            st.write(db_chain.run(query))  # Display the result
+        else:
+            st.write("Please add your OpenAI API key to continue.")
+    else:
+        st.write("Please upload a file first.")
