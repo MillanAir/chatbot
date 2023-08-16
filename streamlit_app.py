dd22b2c54f9951c5623a31deef97ec89b6ac12ba
Milan Verma <millan.verma@gmail.com>
2023-08-16 17:18:00 +0530

    updated streamlit

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,9 @@
 # Import convention
 import streamlit as st
-import numpy as np
 
+st.file_uploader('File uploader')
 with st.chat_message("user"):
     st.write("Hello 👋")
-    st.line_chart(np.random.randn(30, 3))
 
 # Display a chat input widget.
-st.chat_input("Say something")
+st.chat_input("Write your query here...")
